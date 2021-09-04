c91ecb456f47080b851e79b997b8e6453897aea1
Joshua Suskalo <joshua@iris.tv>
2021-09-04 15:51:18 -0500

    Update main to use a resource for build testing

--- src/fermented_formatter/cli/main.clj
+++ src/fermented_formatter/cli/main.clj
@@ -1,8 +1,12 @@
 (ns fermented-formatter.cli.main
   (:require
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
    [fermented-formatter.cli :as tasks])
+  (:import
+   (java.io PushbackReader))
   (:gen-class))
 
 (defn -main
   [& args]
-  )
+  (prn (edn/read (PushbackReader. (io/reader (io/resource "fermented_formatter/config.edn"))))))
